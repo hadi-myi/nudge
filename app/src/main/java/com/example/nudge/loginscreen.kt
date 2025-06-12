3d0238c735b93a1733225b378c71a5368dc2b61e
Hadi <younushadi37@gmail.com>
2025-06-12 11:15:03 -0500

    Visual Tweaks for login screen

--- app/src/main/java/com/example/nudge/loginscreen.kt
+++ app/src/main/java/com/example/nudge/loginscreen.kt
@@ -1,16 +1,33 @@
 package com.example.nudge
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 // creating a login screen
 @Composable
-fun LoginScreen(){
-    Image(
-        painter = painterResource(id = R.drawable.simplenudgelogo),
-        contentDescription = "login"
-    )
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.homescreennudge),
+            contentDescription = "login",
+            modifier = Modifier.size(200.dp)
+        )
+        Text(text = "Take Control - Start Now")
+    }
 }
