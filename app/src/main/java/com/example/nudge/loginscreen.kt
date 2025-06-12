db9992569ffc59e3b5512901c0059c241f68fd02
Hadi <younushadi37@gmail.com>
2025-06-12 11:59:23 -0500

    rearranged layoout for login screen

--- app/src/main/java/com/example/nudge/loginscreen.kt
+++ app/src/main/java/com/example/nudge/loginscreen.kt
@@ -10,10 +10,15 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,19 +26,26 @@ import androidx.compose.ui.text.font.Font
 
 // Custom font variable
 val GeorgeRounded = FontFamily(Font(R.font.georgerounded))
-// creating a login screen
+
 @Composable
 fun LoginScreen() {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.statusBars.asPaddingValues()),  // add status bar padding
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.homescreennudge),
             contentDescription = "login",
-            modifier = Modifier.size(width= 400.dp, height = 200.dp)
+            modifier = Modifier.size(width = 300.dp, height = 200.dp)
         )
-        Text(text = "Reconnect with What Matters", fontSize = 24.sp, fontFamily = GeorgeRounded)
+        Text(
+            text = "Reconnect with What Matters",
+            fontSize = 24.sp,
+            fontFamily = GeorgeRounded,
+            color = colorResource(id = R.color.nudge_blue)
+        )
     }
 }
